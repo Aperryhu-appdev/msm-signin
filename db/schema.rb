1dfd47add7b66f6994bfa78a3c46eb4eea8e072b
Aperryhu <aperryhu@chicagobooth.edu>
2022-09-25 23:18:33 +0000

    User account setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_25_205131) do
+ActiveRecord::Schema.define(version: 2022_09_25_230850) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
