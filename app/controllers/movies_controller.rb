40a1f9874a1c96a060908099ed3c2fb59c2a9a24
Aperryhu <aperryhu@chicagobooth.edu>
2022-09-26 01:06:12 +0000

    Hid user_id in the movies controller instead of fetching the cookie on the details page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,8 @@ class MoviesController < ApplicationController
  
 def add_user_bookmark
   b = Bookmark.new
-  b.user_id = params.fetch("the_user_id")
-  b.movie_id =  params.fetch("the_user_id")
+  b.user_id = session.fetch(:user_id)
+  b.movie_id =  params.fetch("the_movie_id")
   b.save
 
   redirect_to "/"
